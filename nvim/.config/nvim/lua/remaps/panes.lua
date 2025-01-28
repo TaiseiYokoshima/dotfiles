8ff2a3c5930aac009d7cfd976745b6409beac6bc
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-01-28 01:06:08 +0000

    added neovim config to support zellij movement

--- nvim/.config/nvim/lua/remaps/panes.lua
+++ nvim/.config/nvim/lua/remaps/panes.lua
@@ -9,41 +9,8 @@ map("n", "<leader>s", ":vsplit<CR>", opt)
 
 
 
-
-_G.my_move = function(direction)
-   if direction == "left" then
-      direction = "h"
-   elseif direction == "right" then
-      direction = "l"
-   elseif direction == "down" then
-      direction = "j"
-   else
-      direction = "k"
-   end
-
-
-   local id = vim.api.nvim_get_current_win()
-   vim.cmd("wincmd " .. direction)
-   local after = vim.api.nvim_get_current_win()
-
-   local result = after == id
-
-   if result then
-      vim.cmd("echom 'stayed'")
-   else
-
-
-      vim.cmd("echom 'moved'")
-   end
-
-
-end
-
-
-
-
-
 map("n", "<S-Left>", "<C-w>h", opt)
 map("n", "<S-Right>", "<C-w>l", opt)
 map("n", "<S-Up>", "<C-w>k", opt)
 map("n", "<S-Down>", "<C-w>j", opt)
+
